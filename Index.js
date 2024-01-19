0de7d1f2a2c69abe0b81bee12a5ac71ba68ffb0e
MeshackKorir <meshackkorir100@gmail.com>
2024-01-19 10:33:10 +0300

    function counting the vowels

--- Index.js
+++ Index.js
@@ -1,10 +1,33 @@
 // Given a string return a new string with the reversed order of characters
 
-// function reverseThisString(theInput) {
-//     // Using the chaining method
-//     return theInput.split("").reverse().join("");
-// }
+function reverseThisString(theInput) {
+     //I'll use the chaining method connecting the word then reverse then split
+    return theInput.split("").reverse().join("");
+}
 
-// let originalWord = "teach2give";
-// let reversedString = reverseThisString(originalWord);
+let originalWord = "teach2give Is fantastic";
+let reversedString = reverseThisString(originalWord);
 // console.log(reversedString);
+
+
+// write a function that count the number of vowels in a sentence
+
+function vowelCount(sentence)  {
+
+    const vowels = "aeiou"
+
+    const vowelArray = sentence.split("").filter(char => vowels.includes(char))
+
+    return vowelArray.length;
+
+
+}
+
+let sentence = "MeshackKorir"
+let count = vowelCount(sentence);
+
+console.log(count)
+
+//  Using a Prompt allow user to enter numbers separated by commas then print the sum of the numbers  
+
+
