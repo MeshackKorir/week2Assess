a4c0e08a63369a551e17a6da67a57828d09a03a4
MeshackKorir <meshackkorir100@gmail.com>
2024-01-19 10:03:57 +0300

    reverse

---
+++ Index.js
@@ -0,0 +1,10 @@
+// Given a string return a new string with the reversed order of characters
+
+// function reverseThisString(theInput) {
+//     // Using the chaining method
+//     return theInput.split("").reverse().join("");
+// }
+
+// let originalWord = "teach2give";
+// let reversedString = reverseThisString(originalWord);
+// console.log(reversedString);
